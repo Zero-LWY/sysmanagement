c240d528add33181532de1e5a218bb1bd4cd8a0e
Zero灬 <1243426954@163.com>
2020-02-16 16:14:43 +0800

    修改配置

--- src/test/java/com/jluzh/sysmanagement/SysmanagementApplicationTests.java
+++ src/test/java/com/jluzh/sysmanagement/SysmanagementApplicationTests.java
@@ -1,23 +1,24 @@
 package com.jluzh.sysmanagement;
 
-import com.jluzh.sysmanagement.domain.entity.WinvStockLog;
 import com.jluzh.sysmanagement.infra.mapper.WinvStockLogMapper;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 @MapperScan("com.jluzh.sysmanagement.infra.mapper")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 class SysmanagementApplicationTests {
 	@Autowired
 	private WinvStockLogMapper winvStockLogMapper;
+	private final RedisTemplate redisTemplate;
+
 	@Test
 	void contextLoads() {
-		List<WinvStockLog> list = winvStockLogMapper.selectWinvStockLogList();
-		System.out.println(list.size());
+		redisTemplate.opsForValue().set("hello","1");
 	}
 
 }
